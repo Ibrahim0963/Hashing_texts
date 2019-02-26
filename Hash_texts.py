4a514cc81508eaa0284efd7eef3c2affdbf1983c
Ibrahim alakeel <48016716+ibrahimalakeel@users.noreply.github.com>
2019-02-26 20:13:59 +0100

    Add files via upload

---
+++ Hash_texts.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import hashlib 
+print("""
+         _      __              __         _ __ 
+   _____(_)____/ /_  ___  _____/ /_  ___  (_) /_
+  / ___/ / ___/ __ \/ _ \/ ___/ __ \/ _ \/ / __/
+ (__  ) / /__/ / / /  __/ /  / / / /  __/ / /_  
+/____/_/\___/_/ /_/\___/_/  /_/ /_/\___/_/\__/  
+                                                
+""")
+text = input("Enter your text : >>> ")
+print()
+t = hashlib.algorithms_available
+print("\t\t\t## You can choose one of this types ##\n\n",t)
+print()
+Hash_type = input("Enter your hash type : >>>")
+ty = hashlib.new(Hash_type)
+ty.update(b"text")
+print_text = ty.hexdigest()
+print("your text has been hashed >> : "+print_text)
+
+
+
