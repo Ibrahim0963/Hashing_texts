1449e68f801d46d765a7551c0a402c8a2e1ca798
Ibrahim alakeel <48016716+ibrahimalakeel@users.noreply.github.com>
2019-02-26 20:15:42 +0100

    Update Hash_Texts.py

--- Hash_Texts.py
+++ Hash_Texts.py
@@ -7,17 +7,22 @@ print("""
   / ___/ / ___/ __ \/ _ \/ ___/ __ \/ _ \/ / __/
  (__  ) / /__/ / / /  __/ /  / / / /  __/ / /_  
 /____/_/\___/_/ /_/\___/_/  /_/ /_/\___/_/\__/  
-                                                
+               with ibrahim                               
 """)
+
 text = input("Enter your text : >>> ")
 print()
+
 t = hashlib.algorithms_available
+
 print("\t\t\t## You can choose one of this types ##\n\n",t)
 print()
+
 Hash_type = input("Enter your hash type : >>>")
 ty = hashlib.new(Hash_type)
 ty.update(b"text")
 print_text = ty.hexdigest()
+
 print("your text has been hashed >> : "+print_text)
 
 
